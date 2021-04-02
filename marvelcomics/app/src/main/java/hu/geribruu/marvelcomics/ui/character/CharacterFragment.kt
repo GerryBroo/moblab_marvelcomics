569eff36e30396427f06ba5160071a69623c636e
Nemeth Gergely <gergo.nemeth98@gmail.com>
2021-04-02 13:39:52 +0200

    Rename all default files

--- marvelcomics/app/src/main/java/hu/geribruu/marvelcomics/ui/character/CharacterFragment.kt
+++ marvelcomics/app/src/main/java/hu/geribruu/marvelcomics/ui/character/CharacterFragment.kt
@@ -1,4 +1,4 @@
-package hu.geribruu.marvelcomics.ui.notifications
+package hu.geribruu.marvelcomics.ui.character
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import hu.geribruu.marvelcomics.R
 
-class NotificationsFragment : Fragment() {
+class CharacterFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var characterViewModel: CharacterViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        characterViewModel =
+                ViewModelProvider(this).get(CharacterViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_character, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        characterViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
